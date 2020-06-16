5c337e746b77a4139439a46a24b078b66c5b0596
roger <roger@192-168-1-3.tpgi.com.au>
2020-06-17 00:25:17 +1000

    Removed sample project

--- dynadjust/dynadjust/sample/precompile.cpp
+++
@@ -1,6 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-// metadata.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "precompile.h"
-
