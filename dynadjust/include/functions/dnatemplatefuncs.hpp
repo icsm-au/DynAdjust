ba0a36dbdcb846b14b619945e6cbda039e689f1a
Roger Fraser <roger.fraser@delwp.vic.gov.au>
2020-08-19 18:03:51 +1000

    Change n-statistic sorting on GNSS baselines from magnitude of 3 (x, y, z) n-statistic components to maximum (absolute) n-statistic of the 3 components.

--- dynadjust/include/functions/dnatemplatefuncs.hpp
+++ dynadjust/include/functions/dnatemplatefuncs.hpp
@@ -811,7 +811,9 @@ public:
 			case 'G':
 			case 'X':
 			case 'Y':
-				lhsValue = magnitude(_m->at(lhs.first).NStat, _m->at(lhs.first+1).NStat, _m->at(lhs.first+2).NStat);
+				//lhsValue = magnitude(_m->at(lhs.first).NStat, _m->at(lhs.first+1).NStat, _m->at(lhs.first+2).NStat);
+				lhsValue = max(fabs(_m->at(lhs.first).NStat), fabs(_m->at(lhs.first + 1).NStat));
+				lhsValue = max(lhsValue, fabs(_m->at(lhs.first + 2).NStat));
 				break;
 			case 'D':
 				//TRACE("%.9f\n", radians_to_degrees_(_m->at(lhs.first).term1));
@@ -835,7 +837,9 @@ public:
 			case 'G':
 			case 'X':
 			case 'Y':
-				rhsValue = magnitude(_m->at(rhs.first).NStat, _m->at(rhs.first+1).NStat, _m->at(rhs.first+2).NStat);
+				//rhsValue = magnitude(_m->at(rhs.first).NStat, _m->at(rhs.first+1).NStat, _m->at(rhs.first+2).NStat);
+				rhsValue = max(fabs(_m->at(rhs.first).NStat), fabs(_m->at(rhs.first + 1).NStat));
+				rhsValue = max(rhsValue, fabs(_m->at(rhs.first + 2).NStat));
 				break;
 			case 'D':
 				//TRACE("%.9f\n", radians_to_degrees_(_m->at(rhs.first).term1));
@@ -859,7 +863,9 @@ public:
 			case 'G':
 			case 'X':
 			case 'Y':
-				lhsValue = magnitude(_m->at(lhs.first).NStat, _m->at(lhs.first+1).NStat, _m->at(lhs.first+2).NStat);
+				//lhsValue = magnitude(_m->at(lhs.first).NStat, _m->at(lhs.first+1).NStat, _m->at(lhs.first+2).NStat);
+				lhsValue = max(fabs(_m->at(lhs.first).NStat), fabs(_m->at(lhs.first + 1).NStat));
+				lhsValue = max(lhsValue, fabs(_m->at(lhs.first + 2).NStat));
 				break;
 			case 'D':
 				//TRACE("%.9f\n", radians_to_degrees_(_m->at(lhs.first).term1));
@@ -879,7 +885,9 @@ public:
 			case 'G':
 			case 'X':
 			case 'Y':
-				rhsValue = magnitude(_m->at(rhs.first).NStat, _m->at(rhs.first+1).NStat, _m->at(rhs.first+2).NStat);
+				//rhsValue = magnitude(_m->at(rhs.first).NStat, _m->at(rhs.first+1).NStat, _m->at(rhs.first+2).NStat);
+				rhsValue = max(fabs(_m->at(rhs.first).NStat), fabs(_m->at(rhs.first + 1).NStat));
+				rhsValue = max(rhsValue, fabs(_m->at(rhs.first + 2).NStat));
 				break;
 			case 'D':
 				//TRACE("%.9f\n", radians_to_degrees_(_m->at(rhs.first).term1));
