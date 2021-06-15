d1f5d7deabe52bf20a2d39f82de622a250beaf90
Roger Fraser <roger.fraser@delwp.vic.gov.au>
2021-06-15 19:11:39 +1000

    Update make_dynadjust_gcc.sh

--- resources/make_dynadjust_gcc.sh
+++ resources/make_dynadjust_gcc.sh
@@ -282,7 +282,8 @@ echo -e "\n=====================================================================
 # Make!
 if [[ $_binary = "all" ]]; then
 	echo -e "Building DynAdjust $_version...\n"
-	make -j $(nproc) || exit 1
+	#make -j $(nproc) || exit 1
+	make -j 4 || exit 1
 else
 	echo -e "Building DynAdjust (${_binary}) $_version...\n"
 	make -j $(nproc) ${_binary} || exit 1
